957e25ab52e01e2b70a9087e16921d7280dbf88c
mbird <birmr003@mymail.unisa.edu.au>
2014-10-21 21:02:28 +1030

    you can set the destination text now :)

--- DriverlessCar/src/RouteManagement.java
+++ DriverlessCar/src/RouteManagement.java
@@ -9,9 +9,10 @@ import javax.swing.border.EmptyBorder;
 public class RouteManagement extends JPanel {
 
 	private JPanel contentPane;
-	
+	private MenuBar j;
 	private JLabel destinationLabel;
 	private JTextArea destination;
+	private JButton buttonSetDestination;
 	
 	public RouteManagement(JPanel content) {
 		
@@ -31,14 +32,30 @@ public class RouteManagement extends JPanel {
 		
 		JPanel content1 = new JPanel(new GridLayout(3,1,0,40));
 		content1.setMinimumSize(new Dimension(520,720));
-		m.setDestinationText("asdasdasd");
+		
+		buttonSetDestination = new JButton("Set Destination");
+		
+		//j = new MenuBar();
+		j = m;
+		destination = new JTextArea();
 		
 		add(content1);
 		destinationLabel = new JLabel("Destination");
 		destinationLabel.setFont(new Font("Arial", Font.BOLD,48));
 		content1.add(new JLabel("Route Management"));
 		content1.add(destinationLabel);
-		content1.add(new JTextArea());
+		content1.add(destination);
+		content1.add(buttonSetDestination);
+		
+		
+		
+		buttonSetDestination.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+					j.setDestinationText(destination.getText());
+			}
+		});
+		
+		
 	}
 	
 }
